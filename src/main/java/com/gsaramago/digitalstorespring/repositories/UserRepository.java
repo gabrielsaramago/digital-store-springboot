7f90fb565edea78c90e67540cac7264bdb327394
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-03 20:01:57 +0100

    Service layer, component registration

--- src/main/java/com/gsaramago/digitalstorespring/repositories/UserRepository.java
+++ src/main/java/com/gsaramago/digitalstorespring/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.gsaramago.digitalstorespring.repositories;
 
 import com.gsaramago.digitalstorespring.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ src/main/java/com/gsaramago/digitalstorespring/services/UserService.java
@@ -0,0 +1,27 @@
+package com.gsaramago.digitalstorespring.services;
+
+import com.gsaramago.digitalstorespring.model.User;
+import com.gsaramago.digitalstorespring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> opt = userRepository.findById(id);
+        return opt.get();
+    }
+
+}
