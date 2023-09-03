3dc6aa8f22938af565cb2d105eb0dc486a76e69e
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-03 20:34:48 +0100

    add entity Order

---
+++ src/main/java/com/gsaramago/digitalstorespring/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.gsaramago.digitalstorespring.services;
+
+import com.gsaramago.digitalstorespring.model.Order;
+import com.gsaramago.digitalstorespring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+    public Order findById(Long id){
+        return orderRepository.findById(id).get();
+    }
+
+
+}
