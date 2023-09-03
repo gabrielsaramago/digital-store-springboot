40bb0caa402c8a135db8e56d46c36bdb206fcfa7
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-03 18:58:35 +0100

    Project created

---
+++ src/main/java/com/gsaramago/digitalstorespring/DigitalStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gsaramago.digitalstorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  DigitalStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalStoreSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsaramago/digitalstorespring/DigitalStoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsaramago.digitalstorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalStoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
