cb60cbee89fe3ab86d1d3290226a944393ba75db
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-04 12:23:27 +0100

    update category and service association

--- src/main/java/com/gsaramago/digitalstorespring/model/Category.java
+++ src/main/java/com/gsaramago/digitalstorespring/model/Category.java
@@ -1,5 +1,6 @@
 package com.gsaramago.digitalstorespring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -18,7 +19,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @ManyToMany
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
