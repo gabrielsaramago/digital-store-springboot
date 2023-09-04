12476f692becaab9fb515d026a501370bdfc8885
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-04 14:58:39 +0100

    OrderItem ManyToMany association

--- src/main/java/com/gsaramago/digitalstorespring/TestConfig.java
+++ src/main/java/com/gsaramago/digitalstorespring/TestConfig.java
@@ -1,14 +1,8 @@
 package com.gsaramago.digitalstorespring;
 
-import com.gsaramago.digitalstorespring.model.Category;
-import com.gsaramago.digitalstorespring.model.Order;
-import com.gsaramago.digitalstorespring.model.Product;
-import com.gsaramago.digitalstorespring.model.User;
+import com.gsaramago.digitalstorespring.model.*;
 import com.gsaramago.digitalstorespring.model.enums.OrderStatus;
-import com.gsaramago.digitalstorespring.repositories.CategoryRepository;
-import com.gsaramago.digitalstorespring.repositories.OrderRepository;
-import com.gsaramago.digitalstorespring.repositories.ProductRepository;
-import com.gsaramago.digitalstorespring.repositories.UserRepository;
+import com.gsaramago.digitalstorespring.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -70,6 +67,13 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
 
 
