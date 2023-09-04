88a37d685c7ac7e2c118f4d45ec06060e029259b
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-09-04 15:31:28 +0100

    update OrderItem association

--- src/main/java/com/gsaramago/digitalstorespring/model/Product.java
+++ src/main/java/com/gsaramago/digitalstorespring/model/Product.java
@@ -1,5 +1,6 @@
 package com.gsaramago.digitalstorespring.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "Tb_product_category", joinColumns = @JoinColumn(name="product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {
     }
@@ -78,6 +82,13 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        items.stream().forEach(o->set.add(o.getOrder()));
+        return set;
     }
 
 
